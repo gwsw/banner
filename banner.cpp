758731219543a1c6b7b650fbc064972ae5af9f51
Mark Nudelman <markn@greenwoodsoftware.com>
2022-02-24 12:10:07 -0800

    next 6

--- banner.cpp
+++ banner.cpp
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
 #include <string>
 #include <map>
 #include <list>
 #include <vector>
 #include <memory>
+#include <stdexcept>
+
+static char const* sc_clear = "\33[H\33[2J";
+static int quit = 0;
 
 static int usage() {
-    printf("usage: \n");
-    return 1;
+    throw std::runtime_error("usage");
 }
 
 // -----------------------------------------------------------------
@@ -66,13 +70,12 @@ public:
     void resize(int width, int height) {
         if (width < width_) width = width_;
         if (height < height_) height = height_;
-        ///if (width == width_ && height == height_) return;
         CharRect old_cr (*this);
         width_ = width;
         height_ = height;
         bytes_.resize(len());
         clear(fill_);
-        blit(&old_cr, 0, 0, 0, 0, -1, -1, true);
+        blit(&old_cr, 0, 0, 0, 0);
     }
 protected:
     int index(int col, int row) const {
@@ -93,17 +96,23 @@ private:
 class Font {
 public:
     Font(std::string const& filename, char fill = ' ') {
-        if (!parse(filename, fill))
-            throw std::exception();
+        if (!parse_font_file(filename, fill))
+            throw std::runtime_error("cannot parse font file");
     }
     const CharRect* char_image(char ch) const {
-        return lib_.at(ch);
+        try {
+            return lib_.at(ch);
+        } catch (...) {
+            ///return lib_.at('?');
+            fprintf(stderr, "'%c' not in font\n", ch);
+            throw;
+        }
     }
 protected:
     void set_char_image(char ch, const CharRect* img) {
         lib_[ch] = img;
     }
-    bool parse(std::string const& filename, char fill) {
+    bool parse_font_file(std::string const& filename, char fill) {
         FILE* fd = fopen(filename.c_str(), "r");
         if (fd == NULL) {
             fprintf(stderr, "cannot open %s: errno %d\n", filename.c_str(), errno);
@@ -129,6 +138,8 @@ protected:
                 max_len = 0;
                 curr_ch = line[1];
             } else if (line[0] == ' ') {
+                for (char* p = line; *p != '\0'; ++p)
+                    if (*p == '_') *p = ' ';
                 std::string row = std::string(&line[1]);
                 if ((int)row.size() > max_len) max_len = row.size();
                 rows.push_back(row);
@@ -166,27 +177,27 @@ private:
 // -----------------------------------------------------------------
 class Banner {
 public:
-    Banner(std::string const& str, Font const& font) : img_(0,0) {
-        build(str, font);
+    Banner(std::string const& message, Font const& font) : img_(0,0) {
+        build(message, font);
     }
-    void print(int offset, int sc_width, int sc_height, void (*putc)(char ch)) const {
-        for (char const* p = "\33[H\33[J"; *p; ++p)
+    void print(int offset, int sc_width, int sc_height, int xmult, int ymult, void (*putc)(char ch)) const {
+        for (char const* p = sc_clear; *p; ++p)
             (*putc)(*p);
         int rows = std::min(img_.height(), sc_height-1);
         for (int row = 0; row < rows; ++row) {
             for (int col = 0; col < sc_width; ++col) {
-                (*putc)(img_.get_at(col+offset, row));
+                    (*putc)(img_.get_at(col+offset, row));
             }
             (*putc)('\n');
         }
     }
 protected:
-    void build(std::string const& str, Font const& font) {
-        for (size_t i = 0; i < str.size(); ++i) {
-            char ch = str[i];
+    void build(std::string const& message, Font const& font) {
+        for (size_t i = 0; i < message.size(); ++i) {
+            char ch = message[i];
             CharRect const* ch_img = font.char_image(ch);
             int redge = img_.width();
-            img_.resize(img_.width() + ch_img->width(), std::max(img_.height(), ch_img->height()));
+            img_.resize(redge + ch_img->width(), std::max(img_.height(), ch_img->height()));
             img_.blit(ch_img, 0, 0, redge, 0);
         }
     }
@@ -195,67 +206,146 @@ private:
 };
 
 // -----------------------------------------------------------------
+class Params {
+public:
+    Params(int argc, char** argv) {
+        sc_width = atoi(getenv("COLUMNS"));
+        sc_height = atoi(getenv("LINES"));
+        delay_ms = -1;
+        offset_incr = 1;
+        space = 3;
+        xmult = 1;
+        ymult = 1;
+        fill = ' ';
+        color = "";
+        font_file = "?";
+        int ch;
+        while ((ch = getopt(argc, argv, "c:d:f:F:h:i:s:w:x:y:")) != -1) {
+            switch (ch) {
+            case 'c':
+                color = optarg;
+                break;
+            case 'd':
+                delay_ms = atoi(optarg);
+                break;
+            case 'f':
+                font_file = optarg;
+                break;
+            case 'F':
+                fill = optarg[0];
+                break;
+            case 'h':
+                sc_height = atoi(optarg);
+                break;
+            case 'i':
+                offset_incr = atoi(optarg);
+                break;
+            case 's':
+                space = atoi(optarg);
+                break;
+            case 'w':
+                sc_width = atoi(optarg);
+                break;
+            case 'x':
+                xmult = atoi(optarg);
+                break;
+            case 'y':
+                ymult = atoi(optarg);
+                break;
+            default:
+                usage();
+            }
+        }
+        if (optind == argc)
+            usage();
+        for (; optind < argc; ++optind) {
+            if (message != "") message += " ";
+            message += argv[optind];
+        }
+    }
+public:
+    int sc_width;
+    int sc_height;
+    int delay_ms;
+    int offset_incr;
+    int space;
+    int xmult;
+    int ymult;
+    int fill;
+    std::string color;
+    std::string font_file;
+    std::string message;
+};
+
+// -----------------------------------------------------------------
 
 static void putch(char ch) {
     putchar(ch);
 }
 
-static void run_banner(std::string const& str, std::string const& font_file, int delay_ms, int offset_incr, int sc_width, int sc_height, int space, char fill) {
-    struct timespec delay_time;
-    if (delay_ms >= 0) {
-        delay_time.tv_sec = delay_ms / 1000;
-        delay_time.tv_nsec = (delay_ms % 1000) * 1000000;
-    }
-    Font font (font_file);
-    Banner banner(str, font);
-    for (int offset = 0;; offset += offset_incr) {
-        banner.print(offset, sc_width, sc_height, putch);
-        if (delay_ms < 0) break;
-        nanosleep(&delay_time, NULL);
+	static int
+parse_color(char ch)
+{
+	switch (ch)
+	{
+	case 'k': return 30;
+	case 'r': return 31;
+	case 'g': return 32;
+	case 'y': return 33;
+	case 'b': return 34;
+	case 'm': return 35;
+	case 'c': return 36;
+	case 'w': return 37;
+	case 'K': return 90;
+	case 'R': return 91;
+	case 'G': return 92;
+	case 'Y': return 93;
+	case 'B': return 94;
+	case 'M': return 95;
+	case 'C': return 96;
+	case 'W': return 97;
+	default:  return 0;
+	}
+}
+
+static void put_color(std::string const& color) {
+    if (color.size() == 0) {
+        printf("\33[m");
+    } else {
+        int color_fg = parse_color(color[0]);
+        int color_bg = (color.size() <= 1) ? -1 : parse_color(color[1]);
+        if (color_fg > 0)
+            printf("\33[%dm", color_fg);
+        if (color_bg > 0)
+            printf("\33[%dm", color_bg+10);
     }
 }
 
+static void run_banner(Params const& params) {
+    struct timespec delay_time;
+    if (params.delay_ms >= 0) {
+        delay_time.tv_sec = params.delay_ms / 1000;
+        delay_time.tv_nsec = (params.delay_ms % 1000) * 1000000;
+    }
+    Font font (params.font_file);
+    Banner banner(params.message, font);
+    put_color(params.color);
+    for (int offset = 0; !quit; offset += params.offset_incr) {
+        banner.print(offset, params.sc_width, params.sc_height, params.xmult, params.ymult, putch);
+        if (params.delay_ms < 0) break;
+        nanosleep(&delay_time, NULL);
+    }
+    put_color("");
+    printf("%s", sc_clear);
+}
+
+static void intr(int sig) {
+    quit = 1;
+}
+
 int main(int argc, char** argv) {
-    int sc_width = atoi(getenv("COLUMNS"));
-    int sc_height = atoi(getenv("LINES"));
-    int delay_ms = -1;
-    int offset_incr = 1;
-    int space = 3;
-    int fill = ' ';
-    std::string font_file = "?";
-    int ch;
-    while ((ch = getopt(argc, argv, "d:f:F:h:i:s:w:")) != -1) {
-        switch (ch) {
-        case 'd':
-            delay_ms = atoi(optarg);
-            break;
-        case 'f':
-            font_file = optarg;
-            break;
-        case 'F':
-            fill = optarg[0];
-            break;
-        case 'h':
-            sc_height = atoi(optarg);
-            break;
-        case 'i':
-            offset_incr = atoi(optarg);
-            break;
-        case 's':
-            space = atoi(optarg);
-            break;
-        case 'w':
-            sc_width = atoi(optarg);
-            break;
-        default:
-            return usage();
-        }
-    }
-    std::string str = "";
-    for (; optind < argc; ++optind) {
-        if (str != "") str += " ";
-        str += argv[optind];
-    }
-    run_banner(str, font_file, delay_ms, offset_incr, sc_width, sc_height, space, fill);
+    Params params (argc, argv);
+    signal(SIGINT, intr);
+    run_banner(params);
     return 0;
 }
