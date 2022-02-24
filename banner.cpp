1988b6da3265eab5405f3b379c60291f02c87b95
Mark Nudelman <markn@greenwoodsoftware.com>
2022-02-24 12:10:07 -0800

    next 4

--- banner.cpp
+++ banner.cpp
@@ -1,3 +1,25 @@
+
+// ******** ******** ******** ******** ******** ******** ******** ********
+// ******** ******** ******** ******** ******** ******** ******** ********
+// ******** ******** ******** ******** ******** ******** ******** ********
+// ******** 
+// ********    NOTE
+// ******** 
+// ********  It must be (column,row) not (row, column).
+// ******** 
+// ********  That's consistent with (x,y) and with (width,height) order.
+// ******** 
+// ******** 
+// ******** 
+// ******** 
+// ******** 
+// ********  That is all.
+// ******** 
+// ******** 
+// ******** ******** ******** ******** ******** ******** ******** ********
+// ******** ******** ******** ******** ******** ******** ******** ********
+
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -18,42 +40,42 @@ class CharRect {
 public:
     explicit CharRect(int width, int height, char fill = ' ', int kern = 0)
         : width_(width), height_(height), fill_(fill), kern_(kern) {
-		bytes_ = new char[len()];
+        bytes_ = new char[len()];
         clear(fill);
     }
-	explicit CharRect(const CharRect& cr)
-		: width_(cr.width()), height_(cr.height()), fill_(cr.fill()), kern_(cr.kern()) {
-		bytes_ = new char[len()];
-		memcpy(bytes_, cr.bytes_, len());
-	}
+    explicit CharRect(const CharRect& cr)
+        : width_(cr.width()), height_(cr.height()), fill_(cr.fill()), kern_(cr.kern()) {
+        bytes_ = new char[len()];
+        memcpy(bytes_, cr.bytes_, len());
+    }
     int width() const { return width_; }
     int height() const { return height_; }
     char fill() const { return fill_; }
     int kern() const { return kern_; }
-    char get_at(int row, int col) const {
+    char get_at(int col, int row) const {
         if (row >= height_ || width_ == 0) return fill_;
-        return bytes_[index(row, col % width_)];
+        return bytes_[index(col % width_, row)];
     }
-    void set_at(int row, int col, char ch) {
+    void set_at(int col, int row, char ch) {
         if (row >= height_ || col >= width_) return;
-        bytes_[index(row,col)] = ch;
+        bytes_[index(col,row)] = ch;
     }
     void clear(char ch) {
-printf("clear('%c') %d x %d\n", ch, width_, height_);
+//printf("clear('%c') %d x %d\n", ch, width_, height_);
         for (int row = 0; row < height_; ++row) {
             for (int col = 0; col < width_; ++col) {
-                set_at(row, col, ch);
+                set_at(col, row, ch);
             }
         }
     }
-    void blit(const CharRect* const from, int frow, int fcol, int trow, int tcol, int bw = -1, int bh = -1, bool transparent = false) {
+    void blit(const CharRect* const from, int fcol, int frow, int tcol, int trow, int bw = -1, int bh = -1, bool transparent = false) {
         if (bw < 0) bw = from->width();
         if (bh < 0) bh = from->height();
         for (int row = 0; row < bh; ++row) {
             for (int col = 0; col < bw; ++col) {
-                char ch = from->get_at(frow+row, fcol+col);
+                char ch = from->get_at(fcol+col, frow+row);
                 if (!transparent || ch != fill_)
-                    set_at(trow+row, tcol+col, ch);
+                    set_at(tcol+col, trow+row, ch);
             }
         }
     }
@@ -61,35 +83,35 @@ printf("clear('%c') %d x %d\n", ch, width_, height_);
         auto it = rows.begin(); 
         for (int row = 0; row < height_; ++row) {
             for (int col = 0; col < width_; ++col) {
-                set_at(row, col, (it == rows.end() || col >= (int) it->size()) ? fill_ : it->at(col));
-				if (it != rows.end()) ++it;
+                set_at(col, row, (it == rows.end() || col >= (int) it->size()) ? fill_ : it->at(col));
             }
+            if (it != rows.end()) ++it;
         }
     }
     void resize(int width, int height) {
-printf("resize(%d,%d) -> (%d,%d)\n", width_, height_, width, height); fflush(stdout);
-		if (width < width_) width = width_;
-		if (height < height_) height = height_;
-		///if (width == width_ && height == height_) return;
-		CharRect old_cr (*this);
-		free(bytes_);
-		bytes_ = new char[width * height];
+//printf("resize(%d,%d) -> (%d,%d)\n", width_, height_, width, height); fflush(stdout);
+        if (width < width_) width = width_;
+        if (height < height_) height = height_;
+        ///if (width == width_ && height == height_) return;
+        CharRect old_cr (*this);
+        free(bytes_);
+        bytes_ = new char[width * height];
         width_ = width;
         height_ = height;
-		clear(fill_);
-		blit(&old_cr, 0, 0, 0, 0, -1, -1, true);
+        clear(fill_);
+        blit(&old_cr, 0, 0, 0, 0, -1, -1, true);
     }
 protected:
-	int index(int row, int col) const {
+    int index(int col, int row) const {
         return row*width_ + col;
-	}
-	int len() const {
-		return width_ * height_;
-	}
+    }
+    int len() const {
+        return width_ * height_;
+    }
 private:
     int width_;
     int height_;
-	char* bytes_;
+    char* bytes_;
     char fill_;
     int kern_;
 };
@@ -118,10 +140,11 @@ protected:
         while (fgets(line, sizeof(line), fd) != NULL) {
             char * const nl = strchr(line, '\n');
             if (nl != NULL) *nl = '\0';
-			int kern = 0;
-			if (hdr_line(line, &kern)) {
+            int kern = 0;
+            if (hdr_line(line, &kern)) {
                 if (rows.size() > 0) {
-                    CharRect* cr = new CharRect (rows.size(), max_len, fill, kern);
+//printf("--CR (%d,%d)\n", (int)rows.size(), max_len);
+                    CharRect* cr = new CharRect (max_len, rows.size(), fill, kern);
                     cr->init(rows);
                     set_char_image(curr_ch, cr);
                     rows.clear();
@@ -129,33 +152,34 @@ protected:
                 max_len = 0;
                 curr_ch = line[1];
             } else {
-				max_len = std::max(max_len, (int) strlen(line));
+                int len = (int) strlen(line);
+//printf("..(%s) len %d, max %d\n", line, len, max_len);
+                if (len > max_len) max_len = len;
                 rows.push_back(std::string(line));
             }
         }
         fclose(fd);
         return true;
     }
-	bool hdr_line(char const* line, int* kern) {
-		if (line[0] != '=' || line[1] == '\0') return false;
-		for (char const* p = &line[2]; ; ) {
-			while (*p == ' ')
-				++p;
-			if (*p == '\0')
-				break;
-			if (p[1] != '=') {
-				++p;
-			} else {
-				char *ep;
-				int num = (int) strtol(&p[2], &ep, 10);
-				switch (p[0]) {
-				case 'k': *kern = num; break;
-				}
-				p = ep;
-			}
-		}
-		return true;
-	}
+    bool hdr_line(char const* line, int* kern) {
+        if (line[0] != '=' || line[1] == '\0') return false;
+        for (char const* p = &line[2]; ; ) { while (*p == ' ')
+                ++p;
+            if (*p == '\0')
+                break;
+            if (p[1] != '=') {
+                ++p;
+            } else {
+                char *ep;
+                int num = (int) strtol(&p[2], &ep, 10);
+                switch (p[0]) {
+                case 'k': *kern = num; break;
+                }
+                p = ep;
+            }
+        }
+        return true;
+    }
 private:
     std::map<char, const CharRect* > lib_;
 };
@@ -167,12 +191,12 @@ public:
         build(str, font);
     }
     void print(int offset, int sc_width, int sc_height, void (*putc)(char ch)) const {
-//        for (char const* p = "\33[H\33[J"; *p; ++p)
-//            (*putc)(*p);
+        for (char const* p = "\33[H\33[J"; *p; ++p)
+            (*putc)(*p);
         int rows = std::min(img_.height(), sc_height-1);
         for (int row = 0; row < rows; ++row) {
             for (int col = 0; col < sc_width; ++col) {
-                (*putc)(img_.get_at(row, col+offset));
+                (*putc)(img_.get_at(col+offset, row));
             }
             (*putc)('\n');
         }
@@ -199,12 +223,15 @@ static void putch(char ch) {
 
 static void run_banner(std::string const& str, std::string const& font_file, int delay_ms, int offset_incr, int sc_width, int sc_height, int space, char fill) {
     struct timespec delay_time;
-    delay_time.tv_sec = delay_ms / 1000;
-    delay_time.tv_nsec = (delay_ms % 1000) * 1000000;
+    if (delay_ms >= 0) {
+        delay_time.tv_sec = delay_ms / 1000;
+        delay_time.tv_nsec = (delay_ms % 1000) * 1000000;
+    }
     Font font (font_file);
     Banner banner(str, font);
     for (int offset = 0;; offset += offset_incr) {
         banner.print(offset, sc_width, sc_height, putch);
+        if (delay_ms < 0) break;
         nanosleep(&delay_time, NULL);
     }
 }
@@ -212,7 +239,7 @@ static void run_banner(std::string const& str, std::string const& font_file, int
 int main(int argc, char** argv) {
     int sc_width = atoi(getenv("COLUMNS"));
     int sc_height = atoi(getenv("LINES"));
-    int delay = 100;
+    int delay_ms = -1;
     int offset_incr = 1;
     int space = 3;
     int fill = ' ';
@@ -221,7 +248,7 @@ int main(int argc, char** argv) {
     while ((ch = getopt(argc, argv, "d:f:F:h:i:s:w:")) != -1) {
         switch (ch) {
         case 'd':
-            delay = atoi(optarg);
+            delay_ms = atoi(optarg);
             break;
         case 'f':
             font_file = optarg;
@@ -250,6 +277,6 @@ int main(int argc, char** argv) {
         if (str != "") str += " ";
         str += argv[optind];
     }
-    run_banner(str, font_file, delay, offset_incr, sc_width, sc_height, space, fill);
+    run_banner(str, font_file, delay_ms, offset_incr, sc_width, sc_height, space, fill);
     return 0;
 }
